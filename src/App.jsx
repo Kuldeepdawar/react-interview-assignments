4cd28df908191ce844976dd1d6bffc7a1a61db09
KuldeepDawar <kuldeepdawar47@gmail.com>
2025-05-13 12:10:25 +1000

    added debounce with GitHub api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import CrupOperation from "./components/crud/CrupOperation";
+import DebouncedSearch from "./components/debounceSearch";
+import ApnaCode from "./components/ApnaCode";
+import FormValidation from "./components/FormValication";
 
 const App = () => {
   return (
     <div>
-      <CrupOperation />
+      {/* <CrupOperation /> */}
+      {/* <DebouncedSearch /> */}
+      {/* <ApnaCode /> */}
+      <DebouncedSearch />
+      {/* <FormValidation /> */}
     </div>
   );
 };
---
+++ src/components/ApnaCode.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ApnaCode = () => {
+  return <div>ApnaCode</div>;
+};
+
+export default ApnaCode;
